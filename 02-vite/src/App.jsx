ff5d217ae007cdea04a7be449927ae055ec24505
Matthieu Mota <matthieumota@gmail.com>
2023-07-27 09:38:18 +0200

    Overview react router

--- 02-vite/src/App.jsx
+++ 02-vite/src/App.jsx
@@ -1,52 +1,20 @@
-import { useState } from 'react';
-import Button from './Button';
-import Clock from './Clock';
-import List from './List';
-import Text from './Text';
-import State1 from './exercices-state/State1';
-import State2 from './exercices-state/State2';
-import State3 from './exercices-state/State3';
-import StateChallenge from './exercices-state/StateChallenge';
-import Lifecycle1 from './exercices-lifecycle/Lifecycle1';
-import Lifecycle2 from './exercices-lifecycle/Lifecycle2';
-import Lifecycle3 from './exercices-lifecycle/Lifecycle3';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
-  const [show, setShow] = useState(true);
+    return (
+        <>
+            <nav>
+                <Link to="/">Accueil</Link>
+                <Link to="/a-propos">A propos</Link>
+            </nav>
 
-  return (
-    <div>
-      <h1>Vite + React</h1>
-      <Button title="Valider" />
-      <Button title="Ajouter" />
-      <Button title="Envoyer" />
+            <Outlet />
 
-      <h2>Le cours</h2>
-      <Text />
-      <List />
-
-      {show && <Clock timezone="Europe/Paris" />}
-      <button onClick={() => setShow(!show)}>
-        Afficher / Cacher
-      </button>
-      <Clock timezone="Asia/Tokyo" />
-
-      {/*<h2>State 1</h2>
-      <State1 />
-      <h2>State 2</h2>
-      <State2 />
-      <h2>State 3</h2>
-      <State3 />
-      <h2>State challenge</h2>
-      <StateChallenge />*/}
-      <h2>Lifecycle 1</h2>
-      <Lifecycle1 />
-      <h2>Lifecycle 2</h2>
-      <Lifecycle2 />
-      <h2>Lifecycle 3</h2>
-      <Lifecycle3 />
-    </div>
-  );
+            <footer>
+                Mon Footer
+            </footer>
+        </>
+    );
 }
 
 export default App;
---
+++ 02-vite/src/Home.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Button from './Button';
+import Clock from './Clock';
+import List from './List';
+import Text from './Text';
+import State1 from './exercices-state/State1';
+import State2 from './exercices-state/State2';
+import State3 from './exercices-state/State3';
+import StateChallenge from './exercices-state/StateChallenge';
+import Lifecycle1 from './exercices-lifecycle/Lifecycle1';
+import Lifecycle2 from './exercices-lifecycle/Lifecycle2';
+import Lifecycle3 from './exercices-lifecycle/Lifecycle3';
+
+function Home() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <Button title="Valider" />
+      <Button title="Ajouter" />
+      <Button title="Envoyer" />
+
+      <h2>Le cours</h2>
+      <Text />
+      <List />
+
+      {show && <Clock timezone="Europe/Paris" />}
+      <button onClick={() => setShow(!show)}>
+        Afficher / Cacher
+      </button>
+      <Clock timezone="Asia/Tokyo" />
+
+      {/*<h2>State 1</h2>
+      <State1 />
+      <h2>State 2</h2>
+      <State2 />
+      <h2>State 3</h2>
+      <State3 />
+      <h2>State challenge</h2>
+      <StateChallenge />*/}
+      <h2>Lifecycle 1</h2>
+      <Lifecycle1 />
+      <h2>Lifecycle 2</h2>
+      <Lifecycle2 />
+      <h2>Lifecycle 3</h2>
+      <Lifecycle3 />
+    </div>
+  );
+}
+
+export default Home;
